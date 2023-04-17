5418178482c41bf73b00f34d79f53f6aa694e0b4
kfiddle <kenjfiddle@gmail.com>
2023-04-17 15:12:37 -0400

    first commit with small server and google sheet connection

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+
+const { google } = require("googleapis");
+
+const id = "18gnDk9yLRqjN4mk2eCP-Zu9BCt5vvv-3cgCCz7L7Z8w";
+
+const app = express();
+// app.use(express.json());
+
+const authentication = async () => {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+
+  const client = await auth.getClient();
+
+  const sheets = google.sheets({
+    version: "v4",
+    auth: client,
+  });
+
+  return sheets;
+};
+
+const testGet = async () => {
+  try {
+    const sheets = await authentication();
+
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId: id,
+      range: "gigs",
+    });
+    console.log(response.data);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+testGet();
+
+app.use("/", async (req, res) => {
+  try {
+    const { sheets } = await authentication();
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId: id,
+      range: "players",
+    });
+    res.send(response.data);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send();
+  }
+});
+
+app.listen(5000, () => console.log("server is still running on 5000"));
